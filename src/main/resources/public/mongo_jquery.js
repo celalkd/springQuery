941067de1c3ef003e62ed7c760c74ab3495333f8
celalkd <celalkd@192.168.1.24>
2017-03-21 17:31:29 +0300

    jquery-2nd

--- src/main/resources/public/mongo_jquery.js
+++ src/main/resources/public/mongo_jquery.js
@@ -1,16 +1,57 @@
 function search() {
     
-    var input = $("#title").val();
-    var url_input = "http://localhost:8080/find/byTitle?title=" + input;
+    var input = $("#question").val();
+    var list = input.split(/[\s|?|,]+/);    
     
-    $.ajax({type: "GET",
-            url: url_input ,
-            success:function(data){
-                        $('.movie-director').text('Director: '+data.director.toString());                        
-                        $('.movie-rating').text('Rating: '+data.rating);
-                        $('.movie-year').text('Publish Year: '+data.year);
+    
+    var isDirector=false;
+    var isRating=false;
+    var isYear=false;
+    var isGenre=false;
+    var isStarring=false;
+    
+    var titleForUrl = input.match(/"([^"]+)"/)[1];
+    
+    for (var i = 0; i < list.length; i++) {
+        if(list[i]==='director'){
+            isDirector = true;
+        }
+        if(list[i]==='rating'){
+            isRating = true;
+        }
+        if(list[i]==='year' || list[i]==='published'){
+            isYear = true;
+        }
+        if(list[i]==='genre'){
+            isGenre = true;
+        }
+        if(list[i]==='cast'){
+            isStarring = true;
+        }            
+    }
+    if(isDirector===true||isRating===true||isGenre===true||isYear===true||isStarring===true){
+       
+        var url_input = "http://localhost:8080/find/byTitle?title=" + titleForUrl;
+       
+    
+        $.ajax({type: "GET",
+                url: url_input ,
+                success:function(data){
+                    var titleFromData = data.title;
+                    if(isDirector)
+                        $('.movie-director').text(titleFromData+' is directed by '+data.director.toString());      
+                    if(isRating)
+                        $('.movie-rating').text('Rating of the '+titleFromData+' is '+data.rating+'/10');
+                    if(isYear)
+                        $('.movie-year').text(titleFromData+'\'s publish year is '+data.year);
+                    if(isGenre)
                         $('.movie-genre').text('Genre: '+data.genre);
-                        $('.movie-starring').text('Starring: '+data.starring);
+                    if(isStarring)
+                        $('.movie-starring').text('Cast: '+data.starring);
                     }
-                });    
+                });
+    }
+        
+    
+        
 };
